190240ba5eff50417531a4d37393abb51dadbc09
Howard Choi <hchoi84@gmail.com>
2022-08-17 17:59:09 -0700

    Add project files.

---
+++ BlazorFormPractice/Client/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorFormPractice.Shared.Models;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorFormPractice.Client.Pages;
+
+public partial class Index
+{
+  private Person _person = new();
+  private EditContext _editContext = default!;
+  private ValidationMessageStore _messageStore = default!;
+
+  protected override void OnInitialized()
+  {
+    _editContext = new(_person);
+    _messageStore = new(_editContext);
+  }
+}
---
+++ BlazorFormPractice/Shared/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormPractice.Shared.Models;
+public class Person
+{
+  [Required]
+  [Display(Name = "First Name")]
+  public string FirstName { get; set; } = string.Empty;
+
+  [Required]
+  [Display(Name = "Last Name")]
+  public string LastName { get; set; } = string.Empty;
+
+  [Required]
+  [Display(Name = "Gender")]
+  public string Gender { get; set; } = string.Empty;
+
+  [Required]
+  [Display(Name = "Age")]
+  [Range(1, 100)]
+  public int? Age { get; set; }
+
+  [Display(Name = "Height Feet")]
+  [Range(1, 10)]
+  public int? HeightFeet { get; set; }
+
+  [Display(Name = "Height Inch")]
+  [Range(1, 11)]
+  public int? HeightInch { get; set; }
+
+  [Display(Name = "Eye Color")]
+  public string EyeColor { get; set; } = string.Empty;
+}
